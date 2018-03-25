eded84f0edc77bb734065ee0a53a4c67454a7b55
c0llision <0xmw@protonmail.com>
2018-03-26 00:55:43 +0100

    refactored login functions and tests

---
+++ js/login.js
@@ -0,0 +1,30 @@
+var MIN_USERNAME_LENGTH = 4;
+var MIN_PASSWORD_LENGTH = 4;
+
+function createUser(username, password)
+{
+    localStorage.setItem("user-" + username, password);
+}
+
+function checkPassword(username, password)
+{
+    if (localStorage.getItem("user-" + username) === password)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function isUsernameTaken(username) {
+    if (localStorage.getItem("user-" + username) !== null)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
