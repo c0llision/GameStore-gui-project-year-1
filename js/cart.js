20b008af4936800d612168fa6e4e947688559e71
c0llision <0xmw@protonmail.com>
2018-04-03 14:48:21 +0100

    cart is now saved to localStorage

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,9 @@
-cart = [];
+var cart = [];
 
 function addToCart(gameId)
 {
     cart.push(gameId);
+    saveCart();
 }
 
 function isGameInCart(gameId)
@@ -18,4 +19,21 @@ function removeFromCart(index, gameId)
     }
    */ 
     cart.splice(index, 1);
+    saveCart();
 }
+
+function saveCart()
+{
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function loadCart()
+{
+    cart = JSON.parse(localStorage.getItem('cart'));
+    if (cart === null)
+    {
+        cart = []
+    }
+}
+
+loadCart();
