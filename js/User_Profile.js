e72f88ec427558dd4d3f2f13276f9924ac44e8f3
Korzt <G00363134@gmit.ie>
2018-04-05 11:27:00 +0100

    userProfile changes, lists gameID and default length is 0

--- js/User_Profile.js
+++ js/User_Profile.js
@@ -6,26 +6,34 @@
             //unique ID is given, set to index [?][0]
             this.userID = id++;
             //This will make further purchasing of this item impossible
-            this.library = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+            this.library = [];
 
             //update library method, I think depends on how the UI appears, a check list would be simplest 
             this.updateLibrary = function(){
                
-                for (var i = 0; i < this.library.length; i++)
+
+                for (var i = 0; i < gameList.length; i++)
                 {
                     var checkBox = document.getElementById("checkBox0" + i);
                     
                     if
                     (checkBox.checked == true)
                     {
-                        this.library[i] = 1;
+                        if (!(this.library.includes(gameList[i][0])))
+                        {
+                            this.library.push(gameList[i][0]);
+                        }
                     }
                     else
                     {
-                        this.library[i] = 0;
+                        if (this.library.includes(gameList[i][0]))
+                        {
+                            var index = this.library.indexOf(gameList[i][0]);
+                            this.library.splice(index, 1)
+                        }
                     }
                 }
-                localStorage.setItem("library-" + this.userID, JSON.stringify(this.library));
+                localStorage.setItem("library-" + this.username, JSON.stringify(this.library));
 
                 document.getElementById("objTest1").innerHTML = this.toString();
             }
@@ -52,10 +60,10 @@
                 for (var i = 0; i < localStorage.length; i++)
                 {
                     //add to userList array, if begins with "user-"
-                    if (localStorage.key(i).includes("library-" + this.userID))
+                    if (localStorage.key(i).includes("library-" + this.username))
                     {
                         //alert(this.library);
-                         this.library = JSON.parse(localStorage.getItem("library-" + this.userID));
+                         this.library = JSON.parse(localStorage.getItem("library-" + this.username));
                         //this.library = localStorage.getItem();
                     }
                 } 
@@ -63,9 +71,12 @@
 
                 for (var i = 0; i < this.library.length; i++)
                 {
-                    if (this.library[i] == 1)
-                    {
-                       str += gameList[i][1] + "</br>";
+                    for (var j = 0; j < gameList.length; j++){
+
+                        if (this.library[i] == gameList[j][0])
+                        {
+                            str += gameList[j][1] + "</br>"; 
+                        }            
                     }
                 }
                 return str;
