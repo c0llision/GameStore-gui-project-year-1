92d9eb4e1fe7b7d35a988d13235f9c0fd04102bf
c0llision <0xmw@protonmail.com>
2018-04-23 11:54:32 +0100

    rename images

--- js/game.js
+++ js/game.js
@@ -9,16 +9,16 @@ function Game(id, name, price, discount, image) {
 
 // Name, Price, Discount (out of 100), Image
 var gameList = [
-    [100, "PLAYERUNKNOWN'S BATTLEGROUNDS",      29.99,   0,   "imgs/games/PUBG.png"],
-    [101, "Far Cry 5",                          59.99,   0,   "imgs/games/Farcry5.png"],
-    [102, "Ni no Kuni II: Revenant Kingdom",    59.99,   0,   "imgs/games/NinoKuni2.png"],
-    [103, "Grand Theft Auto V",                 59.99,  50,   "imgs/games/GTAV.png"],
-    [104, "Tom Clancy's Ghost Recon Wildlands", 59.99,  67,   "imgs/games/GhostRecon.png"],
-    [105, "Warhammer: Verintide 2",             27.99,   0,   "imgs/games/Vermintide2.png"],
-    [106, "Divinity: Original Sin 2",           44.99,   0,   "imgs/games/DivinityOS2.png"],
-    [107, "Rocket League",                      19.99,  40,   "imgs/games/RocketLeague.png"],
-    [108, "Counter-Strike: Global Offensive",   13.99,   0,   "imgs/games/CounterStrike.png"],
-    [109, "Football Manager 2018",              54.99,  50,   "imgs/games/FootballManager2018.png"]
+    [100, "PLAYERUNKNOWN'S BATTLEGROUNDS",      29.99,   0,   "imgs/games/pubg.png"],
+    [101, "Far Cry 5",                          59.99,   0,   "imgs/games/farcry5.png"],
+    [102, "Ni no Kuni II: Revenant Kingdom",    59.99,   0,   "imgs/games/ninokuni2.png"],
+    [103, "Grand Theft Auto V",                 59.99,  50,   "imgs/games/gtav.png"],
+    [104, "Tom Clancy's Ghost Recon Wildlands", 59.99,  67,   "imgs/games/ghostrecon.png"],
+    [105, "Warhammer: Verintide 2",             27.99,   0,   "imgs/games/vermintide2.png"],
+    [106, "Divinity: Original Sin 2",           44.99,   0,   "imgs/games/divinityos2.png"],
+    [107, "Rocket League",                      19.99,  40,   "imgs/games/rocketleague.png"],
+    [108, "Counter-Strike: Global Offensive",   13.99,   0,   "imgs/games/counterstrike.png"],
+    [109, "Football Manager 2018",              54.99,  50,   "imgs/games/footballmanager2018.png"]
 ];
 
 
